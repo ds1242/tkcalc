7b01d62003ecba31cff063927e055ab616b87f24
David Shaw <dshaw11@utk.edu>
2024-05-09 14:24:38 -0400

    cleanup

--- src/main.py
+++ src/main.py
@@ -9,11 +9,7 @@ def main():
     screen_x = 800
     screen_y = 600
 
-    # win = Tk()
-    # win.geometry(f'{screen_x}x{screen_y}')
     win = Window(screen_x, screen_y)
-    # btn = Button(win, text='click', command=win.destroy)
-    # btn.place(x=100,y=20)
 
     win.add_button('click', button_clicked, 100, 20)
 
