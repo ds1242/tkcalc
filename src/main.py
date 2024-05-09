9be27db78c3da8687f58d7390fa44d85254473f4
David Shaw <dshaw11@utk.edu>
2024-05-09 13:43:27 -0400

    setup the window for the calculator that waits for a close

--- src/main.py
+++ src/main.py
@@ -1,1 +1,12 @@
-print('hello world')
+from graphics import Window
+
+def main():
+    screen_x = 800
+    screen_y = 600
+
+    win = Window(screen_x, screen_y)
+    win.wait_for_close()
+
+main()
+
+
