72642101c2ce885432c41bd4b0177e5263ef9853
David Shaw <dshaw11@utk.edu>
2024-05-09 15:22:44 -0400

    a bit stuck on how to layout the buttons

--- src/main.py
+++ src/main.py
@@ -9,8 +9,12 @@ def main():
     screen_y = 600
 
     win = Window(screen_x, screen_y)
-
-    win.add_button('click', button_clicked, 100, 20)
+    button_symbols = ['0', '1', '2', '3', '4', '5','6', '7','8', '9', '+','-', '*', '/','.']
+    for symbol in button_symbols:
+        
+        win.add_button(symbol, button_clicked, 100, 30)
+    # win.add_button('1', button_clicked, 100, 30)
+    # win.add_button('2', button_clicked, 100, 70)
 
     # test = Buttons(50, 50, "hello", win, button_clicked)
     # test.place(x=50, y=50)
